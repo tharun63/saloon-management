0706d59b6b3019187f3a6051cd7522e40f80bf60
Tharun <tharunrao063@gmail.com>
2022-04-11 17:17:06 +0530

    Update www.js

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env ./node_modules/.bin/babel-node
 
 import app from '../server'
-
-app.listen(3009, function(){
-    console.log('Listening on port 3009');
+app.listen(process.env.PORT, function(){
+    console.log(`Listening on port ${port}`);
 });
