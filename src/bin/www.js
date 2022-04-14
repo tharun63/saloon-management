926690360ffb48a07b8ebf81d1f3afaaa86cce87
Tharun <tharunrao063@gmail.com>
2022-04-14 15:38:36 +0530

    Update www.js

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env ./node_modules/.bin/babel-node
 import app from '../server';
-import dotenv from "dotenv";
+import {} from 'dotenv/config'
 const PORT = 3009
 const port=process.env.PORT || 3009;
 app.listen(port, function(){
