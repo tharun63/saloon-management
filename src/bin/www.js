f713ed74651f74761bf3847e93b1537ffd0b4666
Tharun <tharunrao063@gmail.com>
2022-04-15 11:01:41 +0530

    Delete www.js

--- src/bin/www.js
+++
@@ -1,8 +0,0 @@
-// #!/usr/bin/env ./node_modules/.bin/babel-node
-// import app from '../server';
-// import {} from 'dotenv/config'
-// const PORT = 3009
-// const port=process.env.PORT || 3009;
-// app.listen(port, function(){
-//     console.log(`Listening on port ${port}`);
-// });
